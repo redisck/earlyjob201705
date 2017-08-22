84294bd3ec953b61629300fe941337793653a057
zhufengnodejs <zhufengnodejs@126.com>
2017-08-22 19:28:12 +0800

    login

--- client/containers/Login/index.js
+++ client/containers/Login/index.js
@@ -1,10 +1,19 @@
 import React,{Component} from 'react';
 export default class Login extends Component{
+  constructor(){
+    super();
+    this.state = {username:'',password:''};
+  }
+
   render(){
     return (
-      <div>
-        Login
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="username">用户名</label>
+        <input value={this.state.username} type="text" id="username" onChange={(event)=>this.setState({username:event.target.value})}/>
+        <label htmlFor="password">密码</label>
+        <input value={this.state.password} type="text" id="password" onChange={(event)=>this.setState({password:event.target.value})}/>
+        <input type="submit"/>
+      </form>
     )
   }
 }
